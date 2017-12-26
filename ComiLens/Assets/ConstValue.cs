86e1884cf5505e1a0168ef676060905c70d9f9eb
yata <yatatay777@gmail.com>
2017-12-26 16:43:27 +0900

    ファイル追加

---
+++ ComiLens/Assets/ConstValue.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstValue {
+
+    public static Matrix4x4 GetProjectMatrix()
+    {
+        // HoloLensFaceDetectionOverlayExampleよりコピー
+        //This value is obtained from PhotoCapture's TryGetProjectionMatrix() method.I do not know whether this method is good.
+        //Please see the discussion of this thread.Https://forums.hololens.com/discussion/782/live-stream-of-locatable-camera-webcam-in-unity
+        var projectionMatrix = Matrix4x4.identity;
+        projectionMatrix.m00 = 2.31029f;
+        projectionMatrix.m01 = 0.00000f;
+        projectionMatrix.m02 = 0.09614f;
+        projectionMatrix.m03 = 0.00000f;
+        projectionMatrix.m10 = 0.00000f;
+        projectionMatrix.m11 = 4.10427f;
+        projectionMatrix.m12 = -0.06231f;
+        projectionMatrix.m13 = 0.00000f;
+        projectionMatrix.m20 = 0.00000f;
+        projectionMatrix.m21 = 0.00000f;
+        projectionMatrix.m22 = -1.00000f;
+        projectionMatrix.m23 = 0.00000f;
+        projectionMatrix.m30 = 0.00000f;
+        projectionMatrix.m31 = 0.00000f;
+        projectionMatrix.m32 = -1.00000f;
+        projectionMatrix.m33 = 0.00000f;
+        return projectionMatrix;
+    }
+}
--- ComiLens/Assets/MainScene.cs
+++ ComiLens/Assets/MainScene.cs
@@ -268,27 +268,7 @@ namespace Assets
 
             Debug.Log("Screen.width " + Screen.width + " Screen.height " + Screen.height + " Screen.orientation " + Screen.orientation);
 
-            // HoloLensFaceDetectionOverlayExampleよりコピー
-            //This value is obtained from PhotoCapture's TryGetProjectionMatrix() method.I do not know whether this method is good.
-            //Please see the discussion of this thread.Https://forums.hololens.com/discussion/782/live-stream-of-locatable-camera-webcam-in-unity
-            _projectionMatrix = Matrix4x4.identity;
-            _projectionMatrix.m00 = 2.31029f;
-            _projectionMatrix.m01 = 0.00000f;
-            _projectionMatrix.m02 = 0.09614f;
-            _projectionMatrix.m03 = 0.00000f;
-            _projectionMatrix.m10 = 0.00000f;
-            _projectionMatrix.m11 = 4.10427f;
-            _projectionMatrix.m12 = -0.06231f;
-            _projectionMatrix.m13 = 0.00000f;
-            _projectionMatrix.m20 = 0.00000f;
-            _projectionMatrix.m21 = 0.00000f;
-            _projectionMatrix.m22 = -1.00000f;
-            _projectionMatrix.m23 = 0.00000f;
-            _projectionMatrix.m30 = 0.00000f;
-            _projectionMatrix.m31 = 0.00000f;
-            _projectionMatrix.m32 = -1.00000f;
-            _projectionMatrix.m33 = 0.00000f;
-
+            _projectionMatrix = ConstValue.GetProjectMatrix();
 
             _grayMat = new Mat(webCamTextureMat.rows(), webCamTextureMat.cols(), CvType.CV_8UC1);
             _cascade = new CascadeClassifier();
